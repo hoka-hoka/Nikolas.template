696e9b1117aa56b65ea9c929a9132e6ad87e203e
semyon <sff1@tpu.ru>
2021-05-27 08:18:12 +0700

    produciton

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -1,37 +1,22 @@
-const webpack = require('webpack');
-const { merge } = require('webpack-merge');
-const baseWebpackConfig = require('./webpack.base.config');
-const devserver = require('../webpack/devserver');
+const webpack = require("webpack");
+const { merge } = require("webpack-merge");
+const baseWebpackConfig = require("./webpack.base.config");
+const devserver = require("../webpack/devserver");
 
 const devdWebpackConfig = merge([
   baseWebpackConfig,
   {
-    mode: 'development',
-    /*stats: {
-      assets: false, // скрыть активы из статистики
-      builtAt: false, // скрыть дату сборки
-      cached: false, // информация о кэшировании
-      cachedAssets: false,
-      children: false, // информация о детях
-      chunks: false,
-      chunkGroups: false,
-      chunkModules: false,
-      colors: true, // в разные цвета
-      modules: false // скрыть одули
-    },*/
-    devtool: 'inline-source-map',
+    mode: "development",
+    devtool: "inline-source-map",
     plugins: [
       new webpack.SourceMapDevToolPlugin({
-        filename: '[file].map'
-      })
-    ]
+        filename: "[file].map",
+      }),
+    ],
   },
-  devserver()
-])
+  devserver(),
+]);
 
 module.exports = new Promise((resolve, reject) => {
-	resolve(devdWebpackConfig)
+  resolve(devdWebpackConfig);
 });
-
-
-
